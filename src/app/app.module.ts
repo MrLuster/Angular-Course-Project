05ae0a2377464a4002819d33e1421cf9f41218a3
Damjan <damci4@gmail.com>
2022-12-07 21:06:38 +0100

    Dynamic component section 21 finished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,6 +21,7 @@ import {AuthComponent} from "./auth/auth.component";
 import {LoadingSpinnerComponent} from "./shared/loading-spinner/loading-spinner.component";
 import {AuthInterceptorService} from "./auth/auth-interceptor.service";
 import { AlertComponent } from './shared/alert/alert.component';
+import {PlaceholderDirective} from "./shared/placeholder/placeholder.directive";
 
 @NgModule({
   declarations: [
@@ -37,7 +38,8 @@ import { AlertComponent } from './shared/alert/alert.component';
     RecipeEditComponent,
     AuthComponent,
     LoadingSpinnerComponent,
-    AlertComponent
+    AlertComponent,
+    PlaceholderDirective
 
   ],
   imports: [
@@ -51,6 +53,9 @@ import { AlertComponent } from './shared/alert/alert.component';
     shoppingListService,
     RecipeService,
     {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptorService, multi: true}],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents: [ //na Angular verziji 9 ili više nije potrebno radi i bez toga
+    AlertComponent
+  ]
 })
 export class AppModule { }
